2eaddbfa5712b6a2faa1690e8c22f5d1a082a961
Juha Vierinen <jvierine@gmail.com>
2026-05-20 15:09:53 +0000

    asd

--- mass_fraction.py
+++ mass_fraction.py
@@ -6,7 +6,7 @@ dates=[n.datetime64("2024-10-28T00:00")]
 lat=0
 lon=0
 
-r=n.linspace(40,80,num=200)
+r=n.linspace(50,70,num=200)
 
 data=pymsis.calculate(dates[0],lat,lon,r)
 print(data.shape)
@@ -23,5 +23,5 @@ print(m_total)
 m_spacecraft_influx=1600e3 # kg per year (Schultz et.al., 2026)
 print("influx %1.0f tons per day"%(m_spacecraft_influx/365/1e3))
 
-print("annual space waste mass / atmospheric mass (40-80 km) %1.3f ppt"%(1e12*m_spacecraft_influx/m_total))
+print("annual space waste mass / atmospheric mass (%f-%f km) %1.3f ppt"%(n.min(r),n.max(r),1e12*m_spacecraft_influx/m_total))
 
